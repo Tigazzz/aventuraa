5260f236887ac6de79832aa59924c24337ba2891
Tigazzz <tiago.prazeres.silva@escola.pr.gov.br>
2025-09-11 08:45:28 -0300

    ok

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ avanca.forEach(button => {
   button.addEventListener('click',function(){
    const atual = document.querySelector('.ativo');
    const proximoPasso='passo'+this.getAttribute('data-proximo');
+
+   atual.classList.remove("ativo");
+   const proximoElement=document.getElementById(proximoPasso);
+   if(proximoElemento){
+    proximoElemento.classList.add('ativo');
+ }else{
+  console.error(` Elemento com Id "${proximoPasso}" não encontrado´);
+ }
+
+
+
   })  
-}
-);
+});
